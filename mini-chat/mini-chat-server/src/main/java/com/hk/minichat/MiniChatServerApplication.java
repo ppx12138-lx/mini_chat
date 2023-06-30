e40ac7c7e0f88aa31367c0c557ff2018100d2738
ppx12138 <pp_x12138@163.com>
2023-06-30 17:48:42 +0800

    初始架构搭建

---
+++ mini-chat/mini-chat-server/src/main/java/com/hk/minichat/MiniChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.hk.minichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniChatServerApplication.class, args);
+    }
+
+}
